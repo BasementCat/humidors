e5404df289ae17dda72b6dc8b91108fcd6fbb3c6
Alec Elton <alec.elton@gmail.com>
2017-02-25 21:15:07 -0600

    Run in production with cherrypy

--- webapp/manage.py
+++ webapp/manage.py
@@ -3,6 +3,8 @@
 from flask import url_for
 from flask_script import Manager
 
+import cherrypy
+
 from app import get_app
 
 
@@ -27,6 +29,47 @@ def list_routes():
     
     for line in sorted(output):
         print line
+
+
+@manager.command
+def run_prod(host='127.0.0.1', port=8001, threads=30):
+    # Mount the application
+    cherrypy.tree.graft(app, "/")
+
+    # Unsubscribe the default server
+    cherrypy.server.unsubscribe()
+
+    # Instantiate a new server object
+    server = cherrypy._cpserver.Server()
+
+    # Configure the server object
+    server.socket_host = host
+    server.socket_port = port
+    server.thread_pool = threads
+
+    # For SSL Support
+    # server.ssl_module            = 'pyopenssl'
+    # server.ssl_certificate       = 'ssl/certificate.crt'
+    # server.ssl_private_key       = 'ssl/private.key'
+    # server.ssl_certificate_chain = 'ssl/bundle.crt'
+
+    # Subscribe this server
+    server.subscribe()
+
+    # Example for a 2nd server (same steps as above):
+    # Remember to use a different port
+
+    # server2             = cherrypy._cpserver.Server()
+
+    # server2.socket_host = "0.0.0.0"
+    # server2.socket_port = 8081
+    # server2.thread_pool = 30
+    # server2.subscribe()
+
+    # Start the server engine (Option 1 *and* 2)
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()
 
 
 if __name__ == "__main__":
